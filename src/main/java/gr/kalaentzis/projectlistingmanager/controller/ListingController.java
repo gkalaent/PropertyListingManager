ebad4abcf0c07f9e4df5e9dc03df66f2f6ac9671
kalaentzis <kalaentzis@csd.uoc.gr>
2022-02-20 16:12:51 +0200

    Added validation when adding a new listing to the backend

--- src/main/java/gr/kalaentzis/projectlistingmanager/controller/ListingController.java
+++ src/main/java/gr/kalaentzis/projectlistingmanager/controller/ListingController.java
@@ -4,9 +4,11 @@ import gr.kalaentzis.projectlistingmanager.model.Listing;
 import gr.kalaentzis.projectlistingmanager.model.User;
 import gr.kalaentzis.projectlistingmanager.repository.ListingRepository;
 import gr.kalaentzis.projectlistingmanager.repository.UserRepository;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,12 +43,25 @@ public class ListingController {
     @PostMapping("/{userId}")
     public ResponseEntity addListing(@PathVariable Long userId, @RequestBody Listing listing) {
         Optional<User> user = userRepository.findById(userId);
-        if (user.isPresent()) {
+
+        if (validateListing(listing) && user.isPresent()) {
 
             Listing newListing = new Listing(listing.getCity(), listing.getPrice(), listing.getAvailability(), listing.getArea(), user.get());
             listingRepository.save(newListing);
             return ResponseEntity.ok(newListing);
         }
         return ResponseEntity.badRequest().build();
+    }
+
+    private boolean validateListing(Listing listing){
+        final List<String> validCities = Arrays.asList("Αθήνα", "Θεσσαλονίκη", "Πάτρα", "Ηράκλειο");
+        final List<String> validAvailability = Arrays.asList("Πώληση", "Ενοικίαση");
+        Integer price = listing.getPrice();
+        Integer area = listing.getArea();
+
+        return validCities.contains(listing.getCity()) &&
+                validAvailability.contains(listing.getAvailability()) &&
+                (price >= 50 && price <= 5000000) &&
+                (area >= 20 && area <= 1000);
     }
 }
